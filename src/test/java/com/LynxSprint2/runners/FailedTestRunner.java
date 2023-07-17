6818e37b8b40b23e9d45cdda943ec58acd8329ad
Antonio_Git <l.a.v7232@gmail.com>
2023-07-17 16:23:14 -0500

    Some corrections

--- src/test/java/com/LynxSprint2/runners/FailedTestRunner.java
+++ src/test/java/com/LynxSprint2/runners/FailedTestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "@target/rerun.txt",
-        glue = "com/project_name/step_definitions"
+        glue = "com/LynxSprint2/step_definitions"
 )
 
 public class FailedTestRunner {
