c6fc6bfe6c97a525406698ef5d0cac07c4a9cd15
Antonio_Git <l.a.v7232@gmail.com>
2023-07-18 11:06:40 -0500

    changes

--- src/test/java/com/LynxSprint2/runners/CukesRunner.java
+++ src/test/java/com/LynxSprint2/runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/LynxSprint2/step_definitions",
-        dryRun = true,
+        dryRun = false,
         tags = "",
         publish = true //generating a report with public link
 )
