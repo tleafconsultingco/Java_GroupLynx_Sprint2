5f617e533556a561a09b7ef65fc1dd2ffe8d400d
ferenilay16 <ferenilay16@gmail.com>
2023-07-19 18:54:12 +0300

    MessagePage_updated

--- src/test/java/com/LynxSprint2/pages/MessagePage.java
+++ src/test/java/com/LynxSprint2/pages/MessagePage.java
@@ -1,4 +1,19 @@
 package com.LynxSprint2.pages;
 
+import com.LynxSprint2.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class MessagePage {
+    public MessagePage (){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy (xpath = "//span[@id='feed-add-post-form-tab-message']")
+    public WebElement messageTab;
+
+    @FindBy (xpath="//iframe[@class='bx-editor-iframe']")
+    public WebElement MessageBox;
+
 }
