b2c50b34ed3c396cd2cc06a0bd78bd8b3ad90267
tleafconsultingco <tleaf.consultingco@gmail.com>
2023-07-19 13:19:05 -0500

    updated the login feature, step defs, and page. Now you can use "hr" or "helpdesk" or "marketing" line directly in your scenarios.
    Also updated the marketing username key (it was not consistent with other 2)

--- src/test/java/com/LynxSprint2/step_definitions/LoginStepDefs.java
+++ src/test/java/com/LynxSprint2/step_definitions/LoginStepDefs.java
@@ -1,6 +1,7 @@
 package com.LynxSprint2.step_definitions;
 
 import com.LynxSprint2.pages.LoginPage;
+import com.LynxSprint2.utilities.BrowserUtils;
 import com.LynxSprint2.utilities.ConfigurationReader;
 import com.LynxSprint2.utilities.Driver;
 import io.cucumber.java.en.Given;
@@ -13,21 +14,22 @@ public class LoginStepDefs {
 
     LoginPage loginPage = new LoginPage();
 
-   @Given("the user is on the login page")
+    @Given("the user is on the login page")
     public void the_user_is_on_the_login_page() {
-       Driver.getDriver().get(ConfigurationReader.getProperty("url"));
-    }
-    @When("user is logged in as {string},{string}")
-    public void theUserIsLoggedInAs(String username, String password) {
-        loginPage.login(username,password);
     }
 
-    @When("The user logs in as a {string}")
+    @When("user is logged in as {string},{string}")
+    public void user_is_logged_in_as(String username, String password) {
+        loginPage.login(username,password);
+        BrowserUtils.sleep(2);
+    }
+
+    @When("the user logs in as a {string}")
     public void theUserLogsInAsA(String userType) {
        loginPage.loginAsUserType(userType);
     }
 
-    @Then("The user is on the home page")
+    @Then("the user is on the home page")
     public void theUserIsOnTheHomePage() {
     }
 }
--- src/test/resources/features/login.feature
+++ src/test/resources/features/login.feature
@@ -1,11 +1,10 @@
-@login
+@login @wip
 Feature: User types logged in with valid credentials
 
   Background: User is already on the login page
     Given the user is on the login page
-
+@test
   Scenario Outline: Verify login with different user types
-
     When user is logged in as "<username>","<password>"
     Examples:
       | username                       | password |
@@ -13,14 +12,14 @@ Feature: User types logged in with valid credentials
       | helpdesk1@cybertekschool.com   | UserUser |
       | marketing94@cybertekschool.com | UserUser |
 
-  Scenario: Login as a Driver
-    When The user logs in as a "Hr"
-    Then The user is on the home page
+  Scenario: Login as a Hr
+    When the user logs in as a "Hr"
+    Then the user is on the home page
 
-  Scenario: Login as a Sales Manager
-    When The user logs in as a "Helpdesk"
-    Then The user is on the home page
+  Scenario: Login as a Helpdesk
+    When the user logs in as a "Helpdesk"
+    Then the user is on the home page
 
-  Scenario: Login as a Store Manager
-    When The user logs in as a "Marketing"
-    Then The user is on the home page
+  Scenario: Login as a Marketing
+    When the user logs in as a "Marketing"
+    Then the user is on the home page
