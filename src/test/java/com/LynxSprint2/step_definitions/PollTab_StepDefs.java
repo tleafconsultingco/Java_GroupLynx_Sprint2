3feadd79baaa599817172fbb5693f6e7407f1ff8
doinazosim <Doinazosim91@gmail.com>
2023-07-20 20:29:43 -0500

    some changes

--- src/test/java/com/LynxSprint2/step_definitions/PollTab_StepDefs.java
+++ src/test/java/com/LynxSprint2/step_definitions/PollTab_StepDefs.java
@@ -2,121 +2,93 @@ package com.LynxSprint2.step_definitions;
 
 import com.LynxSprint2.pages.PollTabPage;
 import com.LynxSprint2.utilities.BrowserUtils;
-import com.LynxSprint2.utilities.ConfigurationReader;
 import com.LynxSprint2.utilities.Driver;
-import io.cucumber.java.en.Given;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+import java.util.Map;
 
 public class PollTab_StepDefs {
 
     PollTabPage pollTabPage = new PollTabPage();
 
-    @Given("User is on CRM home page")
-    public void user_is_on_crm_home_page() {
-        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
-    }
 
-    @Given("User is logged in with {string} and {string}")
-    public void user_is_logged_in_with_and(String username, String password) {
-        pollTabPage.login(ConfigurationReader.getProperty("hr_username"),ConfigurationReader.getProperty("hr_password"));
-        }
-
-
-
-
-
-
-    @When("User click Poll")
-    public void user_click_poll() {
+    @When("user clicks {string} button")
+    public void user_clicks_button(String string) {
         pollTabPage.pollButton.click();
-    }
 
+    }
     @Then("user sees All employees by default")
     public void user_sees_all_employees_by_default() {
-        Assert.assertTrue(pollTabPage.allEmployeesBtn.isDisplayed());
+        pollTabPage.allEmployeesBtn.isDisplayed();
+
     }
+
     @When("user clicks Add question")
     public void user_clicks_add_question() {
         pollTabPage.addQuestionBtn.click();
 
     }
-    @Then("user write questions")
-    public void user_write_questions() {
-        pollTabPage.questionTab.sendKeys("question1");
+    @When("user write {string} into the question field")
+    public void user_write_into_the_question_field(String question) {
+        pollTabPage.questionTab.sendKeys(question + Keys.ENTER);
+
     }
-    @Then("user write multiples answers")
-    public void user_write_multiples_answers() {
-        pollTabPage.answerTab1.sendKeys("answer1");
-        pollTabPage.answerTab2.sendKeys("answer2");
+    @When("user write the following answers")
+    public void user_write_the_following_answers(Map<String, String> answers) {
+        pollTabPage.answerTab1.sendKeys("Hello");
+        pollTabPage.answerTab2.sendKeys("World");
     }
+    @Then("user creates poll")
+    public void user_creates_poll() {
+        pollTabPage.sendButton.click();
+    }
+
     @Then("user select Allow multiple choice")
     public void user_select_allow_multiple_choice() {
-        pollTabPage.allowMultChoiceCheckbox.click();
-        pollTabPage.allowMultChoiceCheckbox.isSelected();
-    }
-    @When("user does not write message title")
-    public void user_does_not_write_message_title() {
-        pollTabPage.messageTitle.sendKeys("");
-    }
-    @When("click send button")
-    public void click_send_button() {
-        pollTabPage.sendButton.click();
+        pollTabPage.checkbox.click();
+
     }
 
-    @Then("user sees error message: The message title is not specified")
-    public void user_sees_error_message_the_message_title_is_not_specified() {
-
-        String expectedMessage = "The message title is not specified";
-        String actualMessage = pollTabPage.errorMessage.getText();
-        Assert.assertEquals(expectedMessage,actualMessage);
+    @Then("user sees {string} error message")
+    public void user_sees_error_message(String expectedErrorMsg) {
+        String actualErrorMsg = pollTabPage.errorMessage.getText();
+        Assert.assertTrue(expectedErrorMsg.equals(actualErrorMsg));
     }
 
-    @When("user does not specified recipient")
-    public void user_does_not_specified_recipient() {
-        //pollTabPage.recipientLink.sendKeys("");
+    @When("user clicks on {string} x button")
+    public void user_clicks_on_x_button(String string) {
         pollTabPage.clickXButton.click();
-        BrowserUtils.sleep(3);
-        pollTabPage.sendButton.click();
-    }
-
-    @Then("user sees error message: Please specify at least one person.")
-    public void user_sees_error_message_please_specify_at_least_one_person() {
-        String expectedMsg = "Please specify at least one person.";
-        String actualMsg = pollTabPage.errorMsg.getText();
-        Assert.assertEquals(expectedMsg, actualMsg);
-    }
-
-    @When("user does not write a question")
-    public void user_does_not_write_a_question() {
-        pollTabPage.messageTitle.sendKeys("Cydeo");
-        pollTabPage.messageText.sendKeys("Hello World");
-        pollTabPage.answerTab1.sendKeys("Hello");
-        pollTabPage.sendButton.click();
-    }
-    @Then("user sees error message: The question text is not specified.")
-    public void user_sees_error_message_the_question_text_is_not_specified() {
-        String expectedQErrorMsg = "The question text is not specified.";
-        String actualQErrorMsg = pollTabPage.questionErrorMsg.getText();
-        Assert.assertEquals(expectedQErrorMsg, actualQErrorMsg);
-    }
-    @When("the user does not write the answer")
-    public void the_user_does_not_write_the_answer() {
-        pollTabPage.messageTitle.sendKeys("Cydeo");
-        pollTabPage.messageText.sendKeys("Hello World");
-        pollTabPage.questionTab.sendKeys("How are you?");
-        pollTabPage.sendButton.click();
 
     }
 
-    @Then("user sees error message: The question {string} has no answers.")
-    public void user_sees_error_message_the_question_has_no_answers(String string) {
-        String expectedErrMsg = "The question" + string +" has no answers.";
-        String actualErrMsg = pollTabPage.questionErrorMsg.getText();
-        Assert.assertTrue(actualErrMsg.contains(expectedErrMsg));
+    @And("user write {string} to the answer field")
+    public void userWriteToTheAnswerField(String answer_1) {
+        pollTabPage.answerTab1.sendKeys(answer_1 + Keys.ENTER);
+    }
 
+    @When("user write {string} to the message field")
+    public void user_write_to_the_message_field(String message) {
+        Driver.getDriver().switchTo().frame(Driver.getDriver().findElement(By.xpath("//iframe[@class='bx-editor-iframe']")));
+        pollTabPage.messageText.sendKeys(message + Keys.ENTER);
+        Driver.getDriver().switchTo().defaultContent();
+
+
+    }
+    @And("user write {string} to the question field")
+    public void userWriteToTheQuestionField(String question) {
+        pollTabPage.questionTab.sendKeys(question);
     }
 
 
+    @Then("user sees {string} error message")
+    public void userSeesErrorMessage(String expectedErrorMsg) {
+
+        String actualErrorMsg = pollTabPage.errorMessage.getText();
+        Assert.assertTrue(actualErrorMsg.contains(expectedErrorMsg));
+    }
 }
